1e8e57b41a20a5aa0960c31c9093797d283bc563
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-10-30 15:18:32 +0900

    google login success

--- src/main/java/com/suah/book/springboot/web/IndexController.java
+++ src/main/java/com/suah/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.suah.book.springboot.web;
 
+import com.suah.book.springboot.config.auth.dto.SessionUser;
 import com.suah.book.springboot.service.posts.PostsService;
 import com.suah.book.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,26 +10,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-//    @GetMapping("/")
-//    public String index(){
-//        return "index";   //index.mustache
-//    }
-//
-    @GetMapping("/posts/save")
-    public String postsSave(){
-        return "posts-save";
-    }
-
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 
     @GetMapping("/posts/update/{id}")
